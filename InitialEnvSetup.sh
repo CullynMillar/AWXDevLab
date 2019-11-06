40b37f65906cbb4e9b6e4f41d69791a06162768a
Cullyn Millar <mcullyn@gmail.com>
2019-11-06 23:32:46 +1100

    Initial Commit
    Initial files

---
+++ InitialEnvSetup.sh
@@ -0,0 +1,28 @@
+#Install Python 3.6
+sudo apt-get update -y
+sudo apt-get install python3.6 -y
+
+#Install Git
+sudo apt-get update
+sudo apt-get install git -y
+
+#Install the docker repos
+sudo apt-get update
+sudo apt-get install \
+  apt-transport-https -y\
+  ca-certificates -y\
+  curl -y\
+  software-properties-common -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#Confirm the fingerprint
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+#Install Docker community edition
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+#Add docker python package
+sudo apt-get install python-pip -y
+sudo pip3 install docker
---
+++ Vagrantfile
@@ -0,0 +1,14 @@
+Vagrant.configure("2") do |config|
+  config.vm.provision :"shell", path: 'InitialEnvSetup.sh'
+  config.vm.box = "ubuntu/eoan64"
+  config.vm.network :forwarded_port, guest: 80, host: 8080
+  config.vm.hostname = "AWX-Dev-Box"
+
+  config.vm.provider :virtualbox do |vb|
+      vb.customize [
+        "modifyvm", :id,
+        "--cpuexecutioncap", "50",
+        "--memory", "2046"
+      ]
+    end
+end
